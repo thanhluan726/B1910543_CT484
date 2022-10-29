8e4d68be77dab0268bbec647226b6bfa7ab62f58
B1910543_CT484 <luanb1910543@student.ctu.edu.vn>
2022-10-29 15:41:11 +0700

    xay dung trang cac san pham nguoi dung

--- myshop/lib/main.dart
+++ myshop/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart';
+import 'ui/products/user_products_screen.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
 
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const SafeArea(
-        child: ProductsOverviewScreen(),
+        child: UserProductsScreen(),
       ),
     );
   }
